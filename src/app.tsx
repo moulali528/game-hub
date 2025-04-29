3acc8e7b6970350154851ab06ede8cc504fef63a
Moulali <dudekula.moulali@yahoo.com>
2025-04-29 13:04:46 +0100

    Build Responsive Layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
-import { Button, ButtonGroup } from "@chakra-ui/react"
-import { Input } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Click me</Button>
-      <Input placeholder='Basic usage' />
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "asid main"`,
+      }}>
+        <GridItem area={'nav'} bg={'coral'}>Nav</GridItem>
+        <Show above='lg'>
+          <GridItem area={'asid'} bg={'gold'}>Asid</GridItem>
+        </Show>
+        <GridItem area={'main'} bg={'blue'}>Main</GridItem>
+      </Grid>
 
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { render } from 'preact'
-import './index.css'
 import { App } from './app.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
